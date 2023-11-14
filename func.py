278d2550a2bebfd8eed941236a39b6f98da3d24e
Julien Kolaric <jkolaric@jkolaric56Y96.vmware.com>
2023-11-14 09:50:13 +0100

    test image

--- func.py
+++ func.py
@@ -1,42 +1,74 @@
-from flask import Flask, render_template_string
 import requests
+from PIL import Image
+import numpy as np
 from io import BytesIO
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing.image import img_to_array
+import mlflow
+import os
 
-app = Flask(__name__)
 
-# Route principale
-@app.route('/afficher_image')
-def afficher_image():
-    # URL de l'image à afficher
-    url_image = 'https://www.manutan.fr/fstrz/r/s/www.manutan.fr/img/S/GRP/ST/AIG18043952.jpg?frz-v=96'
+os.environ['MLFLOW_TRACKING_USERNAME'] = 'user'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = 'K1aLXzz0QW'
 
-    # Télécharger l'image à partir de l'URL spécifiée
-    response = requests.get(url_image)
-    
-    # Vérifier si la requête a réussi (code 200)
+# Configuration de MLflow
+mlflow.set_tracking_uri('http://20.67.145.120:80')
+mlflow.set_experiment("image_classification_experiment")
+
+
+
+# Télécharge une image depuis Internet
+def download_image(image_url):
+    response = requests.get(image_url)
     if response.status_code == 200:
-        # Convertir les données binaires de l'image en un objet BytesIO
-        image_data = BytesIO(response.content)
+        img = Image.open(BytesIO(response.content))
+        return img
+    else:
+        return None
 
-        # Charger le modèle HTML avec l'image en paramètre
-        template = '''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Afficher Image</title>
-        </head>
-        <body>
-            <h1>Affichage de l'image</h1>
-            <img src="data:image/png;base64,{{ image_data }}" alt="Image">
-        </body>
-        </html>
-        '''
+# Prédit le contenu de l'image
+def predict_image(model, img):
+    img_resized = img.resize((224, 224))
+    img_array = img_to_array(img_resized)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = preprocess_input(img_array)
 
-        # Rendre le modèle HTML en utilisant Flask
-        return render_template_string(template, image_data=image_data.getvalue().decode('utf-8'))
+    predictions = model.predict(img_array)
+    return decode_predictions(predictions, top=1)[0][0]
 
-    # Si la requête a échoué, retourner une erreur 404
-    return "Erreur: Impossible de récupérer l'image", 404
+def main():
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # URL de l'image
+    image_url = 'https://www.manutan.fr/fstrz/r/s/www.manutan.fr/img/S/GRP/ST/AIG18043952.jpg?frz-v=96'  # Remplacez avec l'URL de l'image que vous souhaitez analyser
+
+    # Enregistrement du processus avec MLflow
+    with mlflow.start_run():
+        # Télécharge et analyse l'image
+        img = download_image(image_url)
+        if img is not None:
+            model = MobileNetV2(weights='imagenet')
+            prediction = predict_image(model, img)
+            
+            # Log information
+            mlflow.log_param("image_url", image_url)
+            mlflow.log_metric("prediction_confidence", float(prediction[2]))
+
+            # Log l'image
+            img.save("predicted_image.jpg")
+            mlflow.log_artifact("predicted_image.jpg")
+            
+            # Log an instance of the trained model for later use
+            mlflow.tensorflow.log_model(model, artifact_path="object-detection")
+
+            # Affiche l'image et la prédiction
+            plt.imshow(img)
+            plt.axis('off')
+            plt.title(f"Prédiction: {prediction[1]} (Confiance: {prediction[2]*100:.2f}%)")
+            plt.show()
+
+            return f"Prédiction: {prediction[1]} (Confiance: {prediction[2]*100:.2f}%)"
+
+        else:
+            print("L'image n'a pas pu être téléchargée.")
